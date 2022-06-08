421da0c57e01b8ad11a33de003b8286fe95bb38c
lukasowy <lukasowy@gmail.com>
2022-06-08 22:18:39 +0200

    1678. Goal Parser Interpretation

---
+++ src/com/company/solutions/GoalParserInterpretation.java
@@ -0,0 +1,28 @@
+package com.company.solutions;
+
+/**
+ * 1678. Goal Parser Interpretation
+ */
+public class GoalParserInterpretation {
+    public String interpret(String command) {
+        StringBuilder sb = new StringBuilder();
+
+        int i = 0;
+        while(i < command.length()){
+            if(command.charAt(i) == 'G'){
+                sb.append("G");
+                i++;
+            } else if(command.charAt(i) == '('){
+                if(command.charAt(i + 1) == ')'){
+                    sb.append("o");
+                    i+=2;
+                } else if(command.charAt(i + 1) == 'a'){
+                    sb.append("al");
+                    i+=4;
+                }
+
+            }
+        }
+        return sb.toString();
+    }
+}
