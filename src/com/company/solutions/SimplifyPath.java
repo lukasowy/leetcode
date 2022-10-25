c29687662099178542c8b5e8b225af91406bd867
Lukasz Sosnowy <lukasowy@gmail.com>
2022-10-25 19:58:58 +0200

    Completed - SimplifyPath

--- src/com/company/solutions/SimplifyPath.java
+++ src/com/company/solutions/SimplifyPath.java
@@ -1,10 +1,34 @@
 package com.company.solutions;
 
+import java.util.ArrayList;
+
 /**
  * 71. Simplify Path
  */
 public class SimplifyPath {
-    public String simplifyPath(String path) {
+    public static String simplifyPath(String path) {
 
+        ArrayList<String> stack = new ArrayList<>();
+        for (String directory : path.replaceFirst("/", "").split("/")) {
+            if (directory.equals("")) {
+                continue;
+            } else if (directory.equals(".")) {
+                continue;
+            } else if (directory.equals("..")) {
+                if (!stack.isEmpty()) {
+                    stack.remove(stack.size() - 1);
+                } else {
+                    continue;
+                }
+            } else {
+                stack.add(directory);
+            }
+        }
+        return "/" + String.join("/", stack);
+    }
+
+    public static void main(String[] args) {
+        String s = simplifyPath("/../");
+        System.out.println(s);
     }
 }
