118f3b397452a89c799d44af211d60cdda1480eb
Lukasz Sosnowy <lukasowy@gmail.com>
2022-10-29 07:34:32 +0200

    Create a depthFirstTraversal method

--- src/com/company/solutions/graphs/GraphTraverser.java
+++ src/com/company/solutions/graphs/GraphTraverser.java
@@ -1,8 +1,29 @@
 package com.company.solutions.graphs;
 
-public class GraphTraverser {
-    
-    public static void main(String[] args) {
+import java.util.ArrayList;
 
+public class GraphTraverser {
+
+    public static void depthFirstTraversal(Vertex start, ArrayList<Vertex> visitedVertices) {
+        System.out.println(start.getData());
+
+        for (Edge edge : start.getEdges()) {
+            Vertex neighbor = edge.getEnd();
+
+            if (!visitedVertices.contains(neighbor)) {
+                visitedVertices.add(neighbor);
+                GraphTraverser.depthFirstTraversal(neighbor, visitedVertices);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        TestGraph test = new TestGraph();
+        Vertex startingVertex = test.getStartingVertex();
+
+        ArrayList<Vertex> visitedVertices = new ArrayList<>();
+        visitedVertices.add(startingVertex);
+
+        depthFirstTraversal(startingVertex, visitedVertices);
     }
 }
