a98d392c4de300240064c71e79c11a3fcb063614
Lukasz Sosnowy <lukasowy@gmail.com>
2022-10-31 08:17:11 +0100

    Test a breadthFirstSearch

--- src/com/company/solutions/graphs/GraphTraverser.java
+++ src/com/company/solutions/graphs/GraphTraverser.java
@@ -17,17 +17,35 @@ public class GraphTraverser {
         }
     }
 
-    public static void breadthFirstSearch(Vertex start, ArrayList<Vertex> visitedVertices){
-        Queue visitedQueue = new Queue();
+    public static void breadthFirstSearch(Vertex start, ArrayList<Vertex> visitedVertices) {
+        Queue visitQueue = new Queue();
+        visitQueue.enqueue(start);
+        while (!visitQueue.isEmpty()) {
+            Vertex current = visitQueue.dequeue();
+            System.out.println(current.getData());
+
+            for (Edge e : current.getEdges()) {
+                Vertex neighbor = e.getEnd();
+                if (!visitedVertices.contains(neighbor)) {
+                    visitedVertices.add(neighbor);
+                    visitQueue.enqueue(neighbor);
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
         TestGraph test = new TestGraph();
         Vertex startingVertex = test.getStartingVertex();
+        ArrayList<Vertex> visitedVertices1 = new ArrayList<>();
+        ArrayList<Vertex> visitedVertices2 = new ArrayList<>();
+        visitedVertices1.add(startingVertex);
+        visitedVertices2.add(startingVertex);
 
-        ArrayList<Vertex> visitedVertices = new ArrayList<>();
-        visitedVertices.add(startingVertex);
+        System.out.println("DFS:");
+        GraphTraverser.depthFirstTraversal(startingVertex, visitedVertices1);
 
-        depthFirstTraversal(startingVertex, visitedVertices);
+        System.out.println("BFS:");
+        GraphTraverser.breadthFirstSearch(startingVertex, visitedVertices2);
     }
 }
